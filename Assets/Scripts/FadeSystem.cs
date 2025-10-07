7ec1dd610534736bdc1873f524633deb240abb5c
juanuchiha1945 <juanuchiha1945@gmail.com>
2025-10-07 13:00:39 +0800

    feat:

---
+++ Assets/Scripts/FadeSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.collections;
+
+/// <summary>
+/// 處理 Canvas Group 畫布群組元件淡入與淡出的系統
+/// </summary>
+public class NewEmptyCSharpScript
+{
+    /// <summary>
+    /// 淡入淡出
+    /// </summary>
+    /// <param name="Group">畫布群組元件</param>
+    /// <param name="fadein">是否淡入</param>
+    /// <param name="interval">淡入淡出時間間隔</param>
+    public void Fade(CanvasGroup Group, bool fadein = true, float interval = 0.03f)
+    {
+
+    }
+}
