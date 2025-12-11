dcb38db56e85e7bd9c1d953a6dd419143e396554
juanuchiha1945 <juanuchiha1945@gmail.com>
2025-12-11 20:13:30 +0800

    feat : 特效製作

--- Assets/Scripts/FSM/PlayerState.cs
+++ Assets/Scripts/FSM/PlayerState.cs
@@ -9,6 +9,7 @@ public class PlayerState : State
     protected Player player;    // 玩家
     protected float inputH;
     protected float inputV;
+    protected float timer;      // 計時器
 
     // 建構函式 : 類別被實例化 new 時會執行，資料初始化處理
     public PlayerState(StateMachine stateMachine, Player player, string name)
@@ -23,6 +24,7 @@ public class PlayerState : State
         base.Enter();
         // 這裡使用了 Rich Text 語法改變顏色
         Debug.Log($"<color=#ff3>進入狀態 : {name}</color>");
+        timer = 0f; // 進入狀態時重置計時器
     }
 
     public override void Exit()
@@ -35,5 +37,8 @@ public class PlayerState : State
         base.Update();
         inputH = Input.GetAxis("Horizontal");   // 獲得玩家水平輸入 A -1，D +1 沒按 0
         inputV = Input.GetAxis("Vertical");     // 獲得玩家垂直輸入 W +1，S -1 沒按 0
+        // 累加時間到計時器內
+        // Time.deltaTime 每個影格的時間，大約是0.02 秒 (1/60)
+        timer += Time.deltaTime;
     }
 }
